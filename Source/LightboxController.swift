79290de31dee09a46a242dcb1610cf2d81e1d40e
Rinat Enikeev <rinat.enikeev@gmail.com>
2017-12-25 15:30:32 +0500

    add loading logic to currentPage didSet

--- Source/LightboxController.swift
+++ Source/LightboxController.swift
@@ -93,6 +93,20 @@ open class LightboxController: UIViewController {
 
       pageDelegate?.lightboxController(self, didMoveToPage: currentPage)
 
+      let pageView = pageViews[currentPage]
+      if pageView.imageView.image == nil && !pageView.isLoadingImage {
+        let lImage = pageView.image
+        if let image = lImage.image {
+          pageView.imageView.image = image
+          pageView.onImageLoaded(image: image)
+        } else if let imageUrl = lImage.imageURL {
+          pageView.isLoadingImage = true
+          LightboxConfig.loadImage(pageView.imageView, imageUrl, { (image) in
+            pageView.onImageLoaded(image: image)
+          })
+        }
+      }
+
       if let image = pageViews[currentPage].imageView.image, dynamicBackground {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.125) {
           self.loadDynamicBackground(image)
